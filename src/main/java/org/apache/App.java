b2f56fb2f562b51854f0150a32760c8e2b1e8894
Martin Radev <martin.b.radev@gmail.com>
2019-07-23 01:21:15 +0200

    Unite paths for f32 and f64

--- src/main/java/org/apache/App.java
+++ src/main/java/org/apache/App.java
@@ -3,30 +3,15 @@ package org.apache;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import org.apache.parquet.hadoop.ParquetFileWriter;
-import org.apache.parquet.schema.MessageType;
+import java.io.FileNotFoundException;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.parquet.schema.Types;
-import org.apache.hadoop.fs.Path;
-import org.apache.parquet.schema.MessageType;
-import org.apache.parquet.schema.MessageTypeParser;
 import org.apache.parquet.hadoop.metadata.CompressionCodecName;
-import org.apache.parquet.column.ColumnDescriptor;
-import org.apache.parquet.column.statistics.BinaryStatistics;
-import org.apache.parquet.bytes.BytesInput;
 import org.apache.parquet.avro.AvroParquetWriter;
 import java.io.IOException;
-import java.util.Random;
 import org.apache.avro.Schema;
-import static org.apache.avro.SchemaBuilder.builder;
 import org.apache.avro.generic.GenericData;
-import static org.apache.parquet.avro.AvroParquetWriter.builder;
-
-import static org.apache.parquet.column.Encoding.BIT_PACKED;
-import static org.apache.parquet.column.Encoding.BYTE_STREAM_SPLIT;
 import org.apache.parquet.hadoop.ParquetWriter;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import org.apache.parquet.avro.AvroParquetReader;
 import org.apache.parquet.hadoop.ParquetReader;
@@ -37,11 +22,11 @@ import org.apache.parquet.hadoop.ParquetReader;
  */
 public class App {
 
-    private static String schema
+    private static String schema64
             = "{ \"type\": \"record\", \"name\": \"asd\", \"doc\": \"something\", \"fields\":"
             + " [{\"name\": \"value\", \"type\": \"double\"}]}";
 
-    public static boolean verify(ArrayList<Double> expected, ArrayList<Double> received) {
+    public static <T> boolean verify(ArrayList<T> expected, ArrayList<T> received) {
         if (expected.size() != received.size()) {
             System.out.println("Sizes do not match.");
             return false;
@@ -54,17 +39,52 @@ public class App {
         return true;
     }
 
-    public static ArrayList<Double> readAll(ParquetReader<GenericData.Record> reader) throws IOException {
-        ArrayList<Double> data = new ArrayList<Double>();
+    public static <T> ArrayList<T> readAll(ParquetReader<GenericData.Record> reader) throws IOException {
+        ArrayList<T> data = new ArrayList<T>();
         GenericData.Record record;
         int cnt = 0;
         while ((record = reader.read()) != null) {
-            data.add((Double) record.get("value"));
+            data.add((T) record.get("value"));
         }
         return data;
     }
+    
+    public static String genName(String baseName, boolean useDict, boolean useByteStream, CompressionCodecName codec, boolean isF32) {
+        String name = baseName;
+        switch(codec) {
+            case UNCOMPRESSED:
+                name += "_no_codec";
+                break;
+            case SNAPPY:
+                name += "_snappy";
+                break;
+            case GZIP:
+                name += "_gzip";
+                break;
+            case ZSTD:
+                name += "_zstd";
+                break;
+            case BROTLI:
+                name += "_brotli";
+                break;
+           case LZO:
+                name += "_brotli";
+                break;
+           default:
+        }
+        if (useByteStream) {
+            name += "_bs";
+        }
+        if (isF32) {
+            name += "_f32";
+        } else {
+            name += "_f64";
+        }
+        name += ".parquet";
+        return name;
+    }
 
-    public static void tryCase(ArrayList<Double> allData, Schema schema, String baseName, boolean useDict, boolean useByteStream, CompressionCodecName codec) throws IOException {
+    public static <T> void tryCase(ArrayList<T> allData, Schema schema, String baseName, boolean useDict, boolean useByteStream, CompressionCodecName codec, boolean isf32) throws IOException {
         String extra = "";
         if (codec == CompressionCodecName.UNCOMPRESSED) {
             extra += "_no_codec";
@@ -76,13 +96,11 @@ public class App {
             extra += "_bs";
         }
         
-        String fName = baseName + "_f64" + extra + ".parquet";
+        String fName = genName(baseName, useDict, useByteStream, codec, isf32);
         if (Files.exists(new File(fName).toPath())) {
             return;
         }
-        //Files.deleteIfExists(new File(fName).toPath());
-        String fullName = baseName + "_f64" + extra + ".parquet";
-        org.apache.hadoop.fs.Path path = new org.apache.hadoop.fs.Path(fullName);
+        org.apache.hadoop.fs.Path path = new org.apache.hadoop.fs.Path(fName);
         ParquetWriter<GenericData.Record> writer = null;
         writer = AvroParquetWriter.
                 <GenericData.Record>builder(path)
@@ -96,14 +114,14 @@ public class App {
                 .withDictionaryEncoding(useDict)
                 .build();
         long startTime = System.currentTimeMillis();
-        for (Double v : allData) {
+        for (T v : allData) {
             GenericData.Record record = new GenericData.Record(schema);
-            record.put("value", v.doubleValue());
+            record.put("value", v);
             writer.write(record);
         }
         writer.close();
         long stopTime = System.currentTimeMillis();
-        System.out.println(fullName + ": " + (stopTime-startTime));
+        System.out.println(fName + ": " + (stopTime-startTime));
 
         // Now check that we can read the data.
         ParquetReader<GenericData.Record> reader = null;
@@ -111,7 +129,7 @@ public class App {
                 .<GenericData.Record>builder(path)
                 .withConf(new Configuration())
                 .build();
-        ArrayList<Double> receivedData = readAll(reader);
+        ArrayList<T> receivedData = readAll(reader);
         if (!verify(allData, receivedData)) {
             System.out.println("Decompression failure.");
         }
@@ -119,21 +137,7 @@ public class App {
 
     public static void main(String[] args) throws IOException {
 
-        /*String[] dataFiles = {
-            "../dataset/32bit/obs_info.sp",
-            "../dataset/32bit/msg_bt.sp",
-            "../dataset/32bit/msg_lu.sp",
-            "../dataset/32bit/msg_sp.sp",
-            "../dataset/32bit/msg_sweep3d.sp",
-            "../dataset/32bit/num_brain.sp",
-            "../dataset/32bit/num_comet.sp",
-            "../dataset/32bit/num_control.sp",
-            "../dataset/32bit/num_plasma.sp",
-            "../dataset/32bit/obs_error.sp",
-            "../dataset/32bit/obs_info.sp",
-            "../dataset/32bit/obs_spitzer.sp",
-            "../dataset/32bit/obs_temp.sp",
-        };*/
+        
         String[] dataFiles = {
             "../dataset/64bit/msg_bt.dp",
             "../dataset/64bit/msg_sppm.dp",
@@ -165,12 +169,51 @@ public class App {
             }
 
             Schema.Parser parser = new Schema.Parser();
-            Schema schema = parser.parse(App.schema);
+            Schema schema = parser.parse(App.schema64);
             // First with no encoding
-            tryCase(allData, schema, baseName, false, false, CompressionCodecName.UNCOMPRESSED);
-            tryCase(allData, schema, baseName, false, false, CompressionCodecName.GZIP);
-            tryCase(allData, schema, baseName, true, false, CompressionCodecName.GZIP);
-            tryCase(allData, schema, baseName, false, true, CompressionCodecName.GZIP);
+            tryCase(allData, schema, baseName, false, false, CompressionCodecName.BROTLI, false);
+            tryCase(allData, schema, baseName, false, true, CompressionCodecName.BROTLI, false);
+            tryCase(allData, schema, baseName, false, false, CompressionCodecName.ZSTD, false);
+            tryCase(allData, schema, baseName, false, true, CompressionCodecName.ZSTD, false);
+        }
+        
+        String[] dataFilesf32 = {
+            "../dataset/32bit/obs_info.sp",
+            "../dataset/32bit/msg_bt.sp",
+            "../dataset/32bit/msg_lu.sp",
+            "../dataset/32bit/msg_sp.sp",
+            "../dataset/32bit/msg_sweep3d.sp",
+            "../dataset/32bit/num_brain.sp",
+            "../dataset/32bit/num_comet.sp",
+            "../dataset/32bit/num_control.sp",
+            "../dataset/32bit/num_plasma.sp",
+            "../dataset/32bit/obs_error.sp",
+            "../dataset/32bit/obs_info.sp",
+            "../dataset/32bit/obs_spitzer.sp",
+            "../dataset/32bit/obs_temp.sp",
+        };
+        
+        for (String dataName : dataFilesf32) {
+            // Read whole file
+            ArrayList<Float> allData = new ArrayList<Float>();
+            String baseName = dataName.split("/")[3];
+            DataInputStream in = new DataInputStream(new FileInputStream(dataName));
+            try {
+                while (true) {
+                    Float v = in.readFloat();
+                    allData.add(v);
+                }
+            } catch (Exception ex) {
+                System.out.println("Num elements: " + Integer.toString(allData.size()));
+            } finally {
+                in.close();
+            }
+
+            Schema.Parser parser = new Schema.Parser();
+            Schema schema = parser.parse(App.schema64);
+            // First with no encoding
+            tryCase(allData, schema, baseName, false, false, CompressionCodecName.ZSTD, true);
+            tryCase(allData, schema, baseName, false, true, CompressionCodecName.ZSTD, true);
         }
     }
 }
